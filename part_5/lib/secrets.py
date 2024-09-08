ce7d0d03db852f7d49145667624a3e18b9a98621
bastelbuud <111197750+bastelbuud@users.noreply.github.com>
2024-09-08 15:04:30 +0200

    Delete part_5 directory

--- part_5/lib/secrets.py
+++
@@ -1,6 +0,0 @@
-WIFIName = "wifi ssid"
-WIFIPassword = "wifi password"
-MQTTBroker = "mqtt broker ip"
-MQTTUser = "mqtt broker user"
-MQTTPassword = "mqtt broker password"
-MQTTClient = "tempsensor-DHT11"  # client must be different for all devices
